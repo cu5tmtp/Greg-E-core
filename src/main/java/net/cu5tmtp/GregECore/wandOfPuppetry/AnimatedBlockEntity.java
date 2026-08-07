399f41d180afe14a42f32efcf63abc7a7812ee48
cu5tmtp <136180264+cu5tmtp@users.noreply.github.com>
2026-08-08 01:35:52 +0200

    remove goalselector

--- src/main/java/net/cu5tmtp/GregECore/wandOfPuppetry/AnimatedBlockEntity.java
+++ src/main/java/net/cu5tmtp/GregECore/wandOfPuppetry/AnimatedBlockEntity.java
@@ -68,10 +68,9 @@ public class AnimatedBlockEntity extends PathfinderMob {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2D, false));
-        this.goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
-        this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
     }
 
     public static AttributeSupplier.Builder createAttributes() {
